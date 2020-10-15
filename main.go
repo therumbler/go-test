3ba54e57de7e75e4f8c40c16d7a0d6fe707e8ae8
Benjamin Rumble <therumbler@gmail.com>
2020-10-15 18:39:38 -0400

    multiple endpoints

--- main.go
+++ main.go
@@ -5,7 +5,16 @@ import (
 )
 
 func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
-  fmt.Fprintf(ctx, "👋 Hi there!  RequestURI is %q", ctx.RequestURI())
+  switch string(ctx.Path()){
+  case "/":
+	  fmt.Fprintf(ctx, "👋 Hi there!  RequestURI is %q", ctx.RequestURI())
+  case "/health":
+	  fmt.Fprintf(ctx, "🏥 Healthy!")
+  case "/liveness":
+	  fmt.Fprintf(ctx, "🎸 Live!")
+  default:
+	ctx.Error("Not found", fasthttp.StatusNotFound)
+  }
 }
 
 func main(){

584a765fb1f1ffec688b645f9341cc404f6354c4
Benjamin Rumble <therumbler@gmail.com>
2020-10-17 09:43:22 -0400

    added log

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
+
 import (
-  "fmt"
-  "github.com/valyala/fasthttp"
+	"fmt"
+	"github.com/valyala/fasthttp"
+	"log"
 )
 
 func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
-  switch string(ctx.Path()){
-  case "/":
-	  fmt.Fprintf(ctx, "👋 Hi there!  RequestURI is %q", ctx.RequestURI())
-  case "/health":
-	  fmt.Fprintf(ctx, "🏥 Healthy!")
-  case "/liveness":
-	  fmt.Fprintf(ctx, "🎸 Live!")
-  default:
-	ctx.Error("Not found", fasthttp.StatusNotFound)
-  }
+	switch string(ctx.Path()) {
+	case "/":
+		fmt.Fprintf(ctx, "👋 Hi there!  RequestURI is %q", ctx.RequestURI())
+	case "/health":
+		fmt.Fprintf(ctx, "🏥 Healthy!")
+	case "/liveness":
+		fmt.Fprintf(ctx, "🎸 Live!")
+	default:
+		ctx.Error("Not found", fasthttp.StatusNotFound)
+	}
 }
 
-func main(){
-  fasthttp.ListenAndServe(":8080", fastHTTPHandler)
+func main() {
+	log.Print("listening on http://127.0.0.1:8080")
+	fasthttp.ListenAndServe(":8080", fastHTTPHandler)
 }
